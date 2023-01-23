224731032f5cee39d7b041d02af48949c1f51664
jasleorez <123354200+jasleorez@users.noreply.github.com>
2023-01-23 16:11:40 +0800

    Add files via upload

---
+++ addNumbers/app.js
@@ -0,0 +1,3 @@
+var add = require ('./add.js');
+var a=10, b=5;
+console.log("Addition: "+add.add(a,b));
---
+++ myGreetings/greet.js
@@ -0,0 +1,8 @@
+x = greet ('John', 'Jane')
+function greet(name1, name2){
+    
+    module.exports = 'Hello ' + name1 + ' and '+name2 
+}
+
+
+ 
